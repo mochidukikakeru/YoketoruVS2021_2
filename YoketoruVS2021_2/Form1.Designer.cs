16722d39df2950e21496b25cbe67e70aa212648f
ookamiyoujyo <ookamiyoujyo@gmail.com>
2021-07-01 10:54:00 +0900

    Initial commit

---
+++ YoketoruVS2021_2/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace YoketoruVS2021_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.gameOverLabel = new System.Windows.Forms.Label();
+            this.clearLabel = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.copyrighLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Yu Gothic UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.titleLabel.Location = new System.Drawing.Point(297, 106);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(158, 37);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "よけとる2021";
+            // 
+            // gameOverLabel
+            // 
+            this.gameOverLabel.AutoSize = true;
+            this.gameOverLabel.Font = new System.Drawing.Font("Yu Gothic UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.gameOverLabel.Location = new System.Drawing.Point(301, 106);
+            this.gameOverLabel.Name = "gameOverLabel";
+            this.gameOverLabel.Size = new System.Drawing.Size(154, 37);
+            this.gameOverLabel.TabIndex = 1;
+            this.gameOverLabel.Text = "ゲームオーバー";
+            // 
+            // clearLabel
+            // 
+            this.clearLabel.AutoSize = true;
+            this.clearLabel.Font = new System.Drawing.Font("Yu Gothic UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.clearLabel.Location = new System.Drawing.Point(339, 106);
+            this.clearLabel.Name = "clearLabel";
+            this.clearLabel.Size = new System.Drawing.Size(77, 37);
+            this.clearLabel.TabIndex = 2;
+            this.clearLabel.Text = "クリア";
+            this.clearLabel.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(127, 37);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Time 100";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(671, 13);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(101, 37);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "★：10";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Yu Gothic UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(276, 190);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(190, 37);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "ハイスコア：100";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(301, 287);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(159, 54);
+            this.startButton.TabIndex = 6;
+            this.startButton.Text = "スタート";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // copyrighLabel
+            // 
+            this.copyrighLabel.AutoSize = true;
+            this.copyrighLabel.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.copyrighLabel.Location = new System.Drawing.Point(315, 401);
+            this.copyrighLabel.Name = "copyrighLabel";
+            this.copyrighLabel.Size = new System.Drawing.Size(129, 17);
+            this.copyrighLabel.TabIndex = 7;
+            this.copyrighLabel.Text = "copyright©2021望月";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.copyrighLabel);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.clearLabel);
+            this.Controls.Add(this.gameOverLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Font = new System.Drawing.Font("Yu Gothic UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Name = "Form1";
+            this.Text = "ハイスコア：100";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label gameOverLabel;
+        private System.Windows.Forms.Label clearLabel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label copyrighLabel;
+    }
+}
+
